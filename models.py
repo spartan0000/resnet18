be8591b564db0896290a6284b003e49578e90129
David Lee <doctor.dlee@gmail.com>
2025-09-29 00:35:53 -0700

    add modified resnet 18

--- models.py
+++ models.py
@@ -1,1 +1,107 @@
 import torch
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from torch import nn
+
+import torchvision
+from torchvision import datasets
+from torchvision.transforms import v2
+
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+
+#hyperparameters
+
+
+#model adjusted for small size of cifar10 images (32x32)
+class Block(nn.Module): 
+    def __init__(self, in_channels: int, out_channels: int, stride: int = 1, downsample = None):
+        super().__init__()
+
+        expansion = 1
+
+        self.relu = nn.ReLU()
+        self.downsample = downsample
+
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size = 3, stride = stride, padding = 1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        identity = x    
+    
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet18(nn.Module):
+    
+    def __init__(self, block, layers, num_classes = 10):
+        super().__init()
+
+        self.in_channels = 64
+
+        #start the model with a modified layer for our small images
+        self.conv1 = nn.Conv2d(3, 64, kernel_size = 1, stride = 1, padding = 1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+
+        #layers
+        self.layer1 = self._make_layer(block, 64, layers[0], stride = 1)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride = 2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride = 2)
+        self.layer4 = self._make_layers(block, 512, layers[3], stride = 2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.fc = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, out_channels, blocks, stride):
+        downsample = None
+
+        if stride != 1 or self.in_channels != out_channels:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels*block.expansion, kernel_size = 1, stride = stride),
+                nn.BatchNorm2d(out_channels*block.expansion)
+            )
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels * block.expansion
+
+        for _ in range(1, blocks):
+            layers.append(block(self.in_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x,1)
+        x = self.fc(x)
+
+        return x
+    
